d6dc13ca3ee8a2f5ebf4f3a8add2417d2cc7601a
binhvu2005 <147390616+binhvu2005@users.noreply.github.com>
2025-02-27 16:42:40 +0700

    ss1

---
+++ src/b2.java
@@ -0,0 +1,5 @@
+public class b2 {
+    public static void main(String[] args) {
+        System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại %s. Quê ở %s","Vũ Thanh Bình",20,"PTIT_HN","Thái Bình"   );
+    }
+}
---
+++ src/b3.java
@@ -0,0 +1,8 @@
+public class b3 {
+    public static void main(String[] args) {
+        System.out.println("bảng cửu chương nhân 4");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("4 * " + i + " = " + (4 * i));
+        }
+    }
+}
